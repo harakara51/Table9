f22417bc8666cc36bcd5839f171b91c09e93563b
Austin Galyon <austingalyon@gmail.com>
2017-11-04 19:30:04 -0400

    Add Tabs and Basic Avatar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import './App.css';
 import Navbar from './component/navbar';
 import MainView from './container/MainView';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {AccountsView} from './test-accounts-view'
+import {AccountsView} from './test-accounts-view';
+import Tabs from './component/tabs';
+
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
       <div className="App">
-        <Navbar name="name"></Navbar>
-        <MainView></MainView>
-        <AccountsView />
+        <Navbar name="name"/>
+        <MainView/>
+        <Tabs/>
+        <AccountsView/>
       </div>
       </MuiThemeProvider>
     );
