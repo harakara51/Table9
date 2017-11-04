3a481285dd0128b49fea78117bb683c2d5e7e4af
Patrick Gilday <6249643+pcgilday@users.noreply.github.com>
2017-11-04 18:17:20 -0400

    Add react-router and unstyled nav (#6)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Navbar from './component/navbar';
-import MainView from './container/MainView';
+import {Navbar} from './component/navbar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {AccountsView} from './test-accounts-view'
+
+
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-      <div className="App">
-        <Navbar name="name"></Navbar>
-        <MainView></MainView>
-        <AccountsView />
-      </div>
+          <Navbar />
       </MuiThemeProvider>
     );
   }
